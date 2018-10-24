81de4689083c898abca76966eb11a4d6e310a706
Hajime Tazaki <thehajime@gmail.com>
2018-10-24 13:59:09 +0900

    darwin: fix mount call for darwin without using union mount
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- snapshots/native/native.go
+++ snapshots/native/native.go
@@ -333,7 +333,7 @@ func (o *snapshotter) mounts(s storage.Snapshot) []mount.Mount {
 	return []mount.Mount{
 		{
 			Source: source,
-			Type:   "bind",
+			Type:   "symlink",
 			Options: []string{
 				roFlag,
 				"rbind",
