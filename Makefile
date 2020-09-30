ukontainer/containerd

commit 3db1c3b8f406edf9e59714d0c65c2fdb37a18d65

Better support windows binaries.
Signed-off-by: Lantao Liu <lantaol@google.com>

Makefile
@@ -106,10 +106,10 @@ GO_GCFLAGS=$(shell				\
 	echo "-gcflags=-trimpath=$${1}/src";	\
 	)
 
+BINARIES=$(addprefix bin/,$(COMMANDS))
+
 #include platform specific makefile
 -include Makefile.$(GOOS)
-
-BINARIES=$(addprefix bin/,$(COMMANDS))
 
 # Flags passed to `go test`
 TESTFLAGS ?= $(TESTFLAGS_RACE)
@@ -178,10 +178,14 @@ benchmark: ## run benchmarks tests
 
 FORCE:
 
+define BUILD_BINARY =
+@echo "$(WHALE) $@"
+@go build ${DEBUG_GO_GCFLAGS} ${GO_GCFLAGS} ${GO_BUILD_FLAGS} -o $@ ${GO_LDFLAGS} ${GO_TAGS}  ./$<
+endef
+
 # Build a binary from a cmd.
 bin/%: cmd/% FORCE
-	@echo "$(WHALE) $@${BINARY_SUFFIX}"
-	@go build ${DEBUG_GO_GCFLAGS} ${GO_GCFLAGS} ${GO_BUILD_FLAGS} -o $@${BINARY_SUFFIX} ${GO_LDFLAGS} ${GO_TAGS}  ./$<
+	$(BUILD_BINARY)
 
 bin/containerd-shim: cmd/containerd-shim FORCE # set !cgo and omit pie for a static shim build: https://github.com/golang/go/issues/17789#issuecomment-258542220
 	@echo "$(WHALE) bin/containerd-shim"

Makefile.windows
@@ -17,9 +17,12 @@
 WHALE = "+"
 ONI = "-"
 
-BINARY_SUFFIX=".exe"
-
 # amd64 supports go test -race
 ifeq ($(GOARCH),amd64)
 	TESTFLAGS_RACE= -race
 endif
+
+BINARIES:=$(addsuffix .exe,$(BINARIES))
+
+bin/%.exe: cmd/% FORCE
+	$(BUILD_BINARY)

commit 5e9916daed220df3480d26d274df01ddcca00e13

darwin: fix to Makefile syntax on darwin
Fixes: 3db1c3b8 ("Better support windows binaries.")
Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

Makefile
@@ -188,7 +188,7 @@ benchmark: ## run benchmarks tests
 
 FORCE:
 
-define BUILD_BINARY =
+define BUILD_BINARY
 @echo "$(WHALE) $@"
 @go build ${DEBUG_GO_GCFLAGS} ${GO_GCFLAGS} ${GO_BUILD_FLAGS} -o $@ ${GO_LDFLAGS} ${GO_TAGS}  ./$<
 endef